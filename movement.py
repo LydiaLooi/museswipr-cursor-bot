4b51a155cc5a5e0b30c4ebff37d453ded64ea755
Lydia Looi <itslydia.looi@gmail.com>
2023-07-11 23:08:47 +1200

    Interruption of movement is working!

--- movement.py
+++ movement.py
@@ -7,19 +7,17 @@ from pytweening import easeInSine
 from multiprocessing import Pool, Manager
 import win32api
 from PIL import ImageGrab
+import threading
 
 
-horizontal_ease_func = easeInSine
-vertical_ease_func = easeInSine
+# horizontal_ease_func = easeInSine
+# vertical_ease_func = easeInSine
 
-horizontal_duration = 0.35
-vertical_duration = 0.35
+# horizontal_duration = 0.35
+# vertical_duration = 0.35
 
-factor = 10000
+# factor = 10000
 
-
-last_left_detection_time = 0
-last_right_detection_time = 0
 
 # Constants for pixel coordinates and expected colors
 LEFT_PIXEL_X = 709
@@ -38,64 +36,128 @@ k_pressed = False
 l_pressed = False
 
 
-def move_mouse(task):
-    direction, h_speed, v_speed = task
-    # print(f"moving {direction}")
-    global last_left_detection_time, last_right_detection_time
+class Command:
+    def execute(self):
+        pass
 
-    # print(f"{time.perf_counter()} Moving {direction}")
 
-    current_x, current_y = win32api.GetCursorPos()  # Get current mouse position
+class MoveLeftCommand(Command):
+    def __init__(self, mouse):
+        self.mouse = mouse
 
-    # Move the mouse with non-linear movement from the current position to the middle of the left side of the screen with randomized y-coordinate
-    screen_width = win32api.GetSystemMetrics(0)
-    screen_height = win32api.GetSystemMetrics(1)
+    def execute(self):
+        self.mouse.start()
+        self.mouse.move_left()
 
-    # Move the mouse with non-linear movement from the current posiqq
+    def stop(self):
+        self.mouse.stop()
 
-    if direction == "left":
-        x = screen_width // 3 + random.randint(
-            -25, 25
-        )  # Move to the third position on the x-axis
-    else:
-        x = screen_width * 2 // 3 + random.randint(
-            -25, 25
-        )  # Move to the two third position on the x-axis
 
-    y_move = random.randint(175, 220)
-    # y_middle = 775  # screen_height // 2 - 100
-    y_middle = screen_height // 2 - 100
-    if current_y > y_middle:
-        y = current_y - y_move
-    else:
-        y = current_y + y_move
+class MoveRightCommand(Command):
+    def __init__(self, mouse):
+        self.mouse = mouse
 
-    # Detect move type? Horizontal vs Vertical
+    def execute(self):
+        self.mouse.start()
+        self.mouse.move_right()
 
-    # Calculate the change in x and y
-    delta_x = abs(x - current_x)
-    delta_y = abs(y - current_y)
+    def stop(self):
+        self.mouse.stop()
 
-    # Determine the move type
-    if delta_x > delta_y:
-        ease_func = horizontal_ease_func
-        duration = horizontal_duration if h_speed is None else h_speed
-    else:
-        ease_func = vertical_ease_func
-        duration = vertical_duration if v_speed is None else v_speed
-        # print("Vertical")
-    # Calculate the duration of movement based on the distance
-    # Perform easing on your own (pytweening's easeInCubic)
-    total_steps = int(duration * factor)
-    for t in range(total_steps):
-        ratio = ease_func(t / total_steps)
 
-        win32api.SetCursorPos(
-            (
-                int(current_x + (x - current_x) * ratio),
-                int(current_y + (y - current_y) * ratio),
+class Mouse:
+    def __init__(self):
+        self.should_stop = False
+        self.screen_width = win32api.GetSystemMetrics(0)
+        self.screen_height = win32api.GetSystemMetrics(1)
+        self.duration = 5
+        self.factor = 10000
+        self.ease_func = easeInSine
+
+    def move_left(self):
+        current_x, current_y = win32api.GetCursorPos()  # Get current mouse position
+
+        # implement the action of moving the mouse to the left
+        # check self.should_stop after each step and stop if it's True
+        x = self.screen_width // 3 + random.randint(-25, 25)
+
+        y_move = random.randint(175, 220)
+        # y_middle = 775  # screen_height // 2 - 100
+        y_middle = self.screen_height // 2 - 100
+        if current_y > y_middle:
+            y = current_y - y_move
+        else:
+            y = current_y + y_move
+
+        total_steps = int(self.duration * self.factor)
+        for t in range(total_steps):
+            ratio = self.ease_func(t / total_steps)
+
+            win32api.SetCursorPos(
+                (
+                    int(current_x + (x - current_x) * ratio),
+                    int(current_y + (y - current_y) * ratio),
+                )
             )
-        )
+            if self.should_stop:
+                print("STOPPPP")
+                break
+
+    def move_right(self):
+        current_x, current_y = win32api.GetCursorPos()  # Get current mouse position
+
+        # implement the action of moving the mouse to the right
+        # check self.should_stop after each step and stop if it's True
+        x = self.screen_width * 2 // 3 + random.randint(-25, 25)
+
+        y_move = random.randint(175, 220)
+        # y_middle = 775  # screen_height // 2 - 100
+        y_middle = self.screen_height // 2 - 100
+        if current_y > y_middle:
+            y = current_y - y_move
+        else:
+            y = current_y + y_move
+
+        total_steps = int(self.duration * self.factor)
+        for t in range(total_steps):
+            ratio = self.ease_func(t / total_steps)
+
+            win32api.SetCursorPos(
+                (
+                    int(current_x + (x - current_x) * ratio),
+                    int(current_y + (y - current_y) * ratio),
+                )
+            )
+            if self.should_stop:
+                break
+
+    def stop(self):
+        self.should_stop = True
+
+    def start(self):
+        self.should_stop = False
+
+
+class Invoker:
+    def __init__(self, mouse: Mouse):
+        self.mouse = mouse
+        self.command = None
+        self.current_thread = None
+
+    def set_command(self, command: Command):
+        # If there is a current command being executed, stop it
+        if self.command:
+            self.command.stop()
+
+        self.command = command
+
+        # Start a new thread to execute the command
+        self.current_thread = threading.Thread(target=self.execute_command)
+        self.current_thread.start()
+
+    def execute_command(self):
+        if self.command:
+            self.command.execute()
 
 
 def print_detection(task):
@@ -117,13 +179,13 @@ def check_and_move(queue):
             quit()
 
         if keyboard.is_pressed("a") and not a_pressed:
-            queue.put(("left", None, None))
+            queue.put(("left",))
             a_pressed = True
         elif not keyboard.is_pressed("a"):
             a_pressed = False
 
         if keyboard.is_pressed("s") and not s_pressed:
-            queue.put(("right", None, None))
+            queue.put(("right",))
             s_pressed = True
         elif not keyboard.is_pressed("s"):
             s_pressed = False
@@ -131,8 +193,10 @@ def check_and_move(queue):
 
 if __name__ == "__main__":
     print("Running...")
-    print(f"Horizontal total steps is: {int(horizontal_duration * factor)}")
-    print(f"Vertical total steps is: {int(vertical_duration * factor)}")
+
+    # Create an instance of Mouse and Invoker
+    mouse = Mouse()
+    invoker = Invoker(mouse)
 
     with Manager() as manager:
         task_queue = manager.Queue()
@@ -140,4 +204,14 @@ if __name__ == "__main__":
             pool.apply_async(check_and_move, (task_queue,))
             while True:
                 task = task_queue.get()
-                pool.apply_async(print_detection, (task,))
+                direction = task[0]
+                if direction == "left":
+                    invoker.set_command(
+                        MoveLeftCommand(mouse)
+                    )  # Set the new command to move left
+                elif direction == "right":
+                    invoker.set_command(
+                        MoveRightCommand(mouse)
+                    )  # Set the new command to move right
+                else:
+                    print(f"Something went wrong: {task}")
