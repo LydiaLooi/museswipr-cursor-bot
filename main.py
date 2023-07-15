6e960c33803ae2bdf5abdbb5b33d188660319dcd
Lydia Looi <itslydia.looi@gmail.com>
2023-07-15 19:02:14 +1200

    Updated slow values to be less slow but still good. Added some perf checking stuff and a button for starting the detection

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ DEBOUNCE_DELAY = config["DEBOUNCE_DELAY"]
 def check_and_move(queue, local_mem):
     while True:
         current_time = perf_counter()
+        local_mem["check_count"] += 1
 
         for direction in ["left", "right"]:
             pixel_x = LEFT_PIXEL_X if direction == "left" else RIGHT_PIXEL_X
@@ -44,30 +45,47 @@ def check_and_move(queue, local_mem):
                     # Overrides the last slow swipe if this next one is gonna be faster
                     if not local_mem["faster"]:
                         local_mem["faster"] = True
+
                 queue.put((direction, local_mem["faster"]))
                 local_mem[direction] = current_time
                 local_mem["faster"] = faster
 
 
 if __name__ == "__main__":
-    print("Running...")
-
+    print("Starting... press 'e' to begin detecting.")
     # Create an instance of Mouse and Invoker
     mouse = Mouse()
     invoker = MouseMoveInvoker(mouse)
     set_command = invoker.set_command
+    start = False
+    while start is False:
+        if is_pressed("e"):
+            start = True
+            print("Detection has begun.\n")
+
+    start_time = perf_counter()
 
     try:
         with Manager() as manager:
             task_queue = manager.Queue()
-            local_mem = manager.dict({"left": 0, "right": 0, "faster": True})
+            local_mem = manager.dict(
+                {"left": 0, "right": 0, "faster": True, "check_count": 0}
+            )
 
-            with Pool(processes=1) as pool:  # Create a pool of 6 worker processes
+            with Pool(processes=1) as pool:
                 pool.apply_async(check_and_move, (task_queue, local_mem))
                 while True:
                     if is_pressed("q"):
-                        print("Quitting...")
+                        end_time = perf_counter()
+                        time_ran = end_time - start_time
+                        print(f"Quitting... Was detecting for {time_ran:.2f}s ")
+                        checks_s = local_mem["check_count"] / time_ran
+                        print(
+                            f"Check count: {local_mem['check_count']} | Checks/s: {checks_s}"
+                        )
                         exit(0)
+                    elif is_pressed("e"):
+                        print(local_mem)
                     task = task_queue.get()
                     direction, faster = task
                     if direction == "left":
